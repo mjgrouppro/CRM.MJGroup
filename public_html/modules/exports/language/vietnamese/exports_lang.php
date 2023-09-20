a423af5e715591bcabbb32695a72d1d284a729f5
giathai2605 <chiyeu1nguoi.2003@gmail.com>
2023-09-20 14:23:24 +0700

    first commit

---
+++ public_html/modules/exports/language/vietnamese/exports_lang.php
@@ -0,0 +1,12 @@
+<?php
+$lang['csv_export'] = 'Xuất CSV';
+$lang['csv_exports'] = 'Xuất CSV';
+$lang['contacts'] = 'Thông tin liên hệ';
+$lang['csv_export_select_type'] = 'Loại Xuất';
+$lang['csv_export_all_time'] = 'Tất cả thời gian';
+$lang['csv_export_three_months'] = '3 tháng qua';
+$lang['csv_export_six_months'] = '6 tháng qua';
+$lang['csv_export_twelve_months'] = '12 tháng qua';
+$lang['csv_export_from_date'] = 'Từ ngày';
+$lang['csv_export_to_date'] = 'Đến ngày';
+$lang['csv_export_button'] = 'Xuất';
